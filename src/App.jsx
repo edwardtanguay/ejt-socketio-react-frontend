492242533013997aa5912f592c994d3b6220e403
Edward Tanguay (windows) <edward@tanguay.info>
2022-07-07 21:07:54 +0200

    basic connection works

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import { useEffect } from 'react';
 import './App.scss';
+import { initiateSocketConnection } from './socketio.service';
 
 function App() {
+
+	useEffect(() => {
+		initiateSocketConnection();
+	}, []);
+
 	return (
 		<div className="App">
 			<h1>Socket.io frontend</h1>
---
+++ src/socketio.service.js
@@ -0,0 +1,8 @@
+import { io } from 'socket.io-client';
+
+let socket;
+
+export const initiateSocketConnection = () => {
+	socket = io(import.meta.env.VITE_SOCKET_ENDPOINT);
+	console.log(`Connecting socket...`);
+};
